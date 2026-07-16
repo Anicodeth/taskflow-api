b3c3f89b1d42afdf51403216bfcc5b74a9396505
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:08:01 +0300

    test(envx): lookups and fallbacks

---
+++ internal/envx/envx_test.go
@@ -0,0 +1,23 @@
+﻿package envx
+
+import (
+	"os"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	os.Setenv("ENVX_T", "hi")
+	if String("ENVX_T", "x") != "hi" {
+		t.Fatal("string")
+	}
+	if String("ENVX_MISSING", "x") != "x" {
+		t.Fatal("fallback")
+	}
+}
+
+func TestInt(t *testing.T) {
+	os.Setenv("ENVX_N", "42")
+	if Int("ENVX_N", 0) != 42 {
+		t.Fatal("int")
+	}
+}
