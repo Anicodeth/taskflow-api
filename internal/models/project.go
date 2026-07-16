0443c22169b8a5312085fc9d9f34a09a14c60cb1
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 15:44:09 +0300

    feat(models): add project domain type

---
+++ internal/models/project.go
@@ -0,0 +1,11 @@
+﻿package models
+
+import "time"
+
+// Project is a TaskFlow project resource.
+type Project struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+}
