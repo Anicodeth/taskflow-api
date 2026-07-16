ca4394526ceb4a9a0235ccbcf5cd2e4ce816857b
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 15:47:25 +0300

    feat(models): add tag domain type

---
+++ internal/models/tag.go
@@ -0,0 +1,11 @@
+﻿package models
+
+import "time"
+
+// Tag is a TaskFlow tag resource.
+type Tag struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+}
