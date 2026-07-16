4d49b77ea5039a2feddefb57041a22d8ab64e321
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:02:18 +0300

    feat(models): add attachment domain type

---
+++ internal/models/attachment.go
@@ -0,0 +1,11 @@
+﻿package models
+
+import "time"
+
+// Attachment is a TaskFlow attachment resource.
+type Attachment struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+}
