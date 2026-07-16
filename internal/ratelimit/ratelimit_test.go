ae8d294704b284c7c577a51fe7a5ae227f1f0b0b
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:16:25 +0300

    test(ratelimit): allow then deplete

---
+++ internal/ratelimit/ratelimit_test.go
@@ -0,0 +1,17 @@
+﻿package ratelimit
+
+import "testing"
+
+func TestBucket(t *testing.T) {
+	b := NewBucket(2)
+	if !b.Allow() || !b.Allow() {
+		t.Fatal("should allow 2")
+	}
+	if b.Allow() {
+		t.Fatal("should be empty")
+	}
+	b.Refill()
+	if !b.Allow() {
+		t.Fatal("refill")
+	}
+}
