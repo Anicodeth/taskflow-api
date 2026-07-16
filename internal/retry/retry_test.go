d4d9ef11f8532c6d432a339b45282a4deca53e2a
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:08:19 +0300

    test(retry): succeeds eventually

---
+++ internal/retry/retry_test.go
@@ -0,0 +1,23 @@
+﻿package retry
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestDoSucceedsEventually(t *testing.T) {
+	n := 0
+	err := Do(3, func() error {
+		n++
+		if n < 2 {
+			return errors.New("nope")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if n != 2 {
+		t.Fatalf("n=%d", n)
+	}
+}
