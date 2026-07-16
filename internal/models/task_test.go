b54aee75243e68b1f09a1c77ac2595dbacc7354e
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:13:04 +0300

    test(models): status validation

---
+++ internal/models/task_test.go
@@ -0,0 +1,12 @@
+﻿package models
+
+import "testing"
+
+func TestStatusValid(t *testing.T) {
+	if !StatusTodo.Valid() {
+		t.Fatal("todo should be valid")
+	}
+	if Status("bogus").Valid() {
+		t.Fatal("bogus should be invalid")
+	}
+}
