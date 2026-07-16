af11921d4ccecc0a1ee91ed24414fc556f723bed
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:01:12 +0300

    feat(models): add label domain type

---
+++ internal/models/label.go
@@ -0,0 +1,11 @@
+﻿package models
+
+import "time"
+
+// Label is a TaskFlow label resource.
+type Label struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+}
