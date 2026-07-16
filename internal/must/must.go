6cd83c25465905c85135ce3e2cb3cd64cf734994
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:09:28 +0300

    feat(must): panic-on-error helper

---
+++ internal/must/must.go
@@ -0,0 +1,10 @@
+﻿// Package must provides panic-on-error helpers for initialisation code.
+package must
+
+// Get returns v or panics if err is non-nil.
+func Get[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
