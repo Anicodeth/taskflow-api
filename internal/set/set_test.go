831253cd5f01753f99f2d33e5c039307ae700f05
Anicodeth <13ananya13bb@gmail.com>
2026-07-16 16:10:05 +0300

    test(set): membership and size

---
+++ internal/set/set_test.go
@@ -0,0 +1,17 @@
+﻿package set
+
+import "testing"
+
+func TestSet(t *testing.T) {
+	s := New(1, 2, 2, 3)
+	if s.Len() != 3 {
+		t.Fatalf("len=%d", s.Len())
+	}
+	if !s.Has(2) {
+		t.Fatal("has")
+	}
+	s.Add(4)
+	if !s.Has(4) {
+		t.Fatal("add")
+	}
+}
